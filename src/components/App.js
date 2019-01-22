50d03897c8c695f00f33bf2e672319aaa6a58f2f
Michael Freytag <michaelfreytag@nanonation.net>
2019-01-22 15:48:15 -0600

    Add AddFishForm.js; Set Initial State and add fish to state
    Simplify ESlint rules

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,22 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    // 1. copy existing state;
+    const fishes = { ...this.state.fishes };
+    // 2. add new fish to existing state
+    fishes[`fish_${Date.now()}`] = fish;
+    // 3. set new state;
+    this.setState({
+      fishes
+    });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +27,7 @@ class App extends Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import AddFishForm from './AddFishForm';
 
 class Inventory extends Component {
   render() {
-    return <div className="inventory">Inventories</div>;
+    return (
+      <div className="inventory">
+        <h2>Inventory</h2>
+        <AddFishForm addFish={this.props.addFish} />
+      </div>
+    );
   }
 }
 
