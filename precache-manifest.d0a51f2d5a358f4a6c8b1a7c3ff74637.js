617b995f09b9dba9de6ed10fe6332c71319042e8
Michael Freytag <mtfreytag@gmail.com>
2019-01-23 21:22:57 -0600

    Updates

---
+++ precache-manifest.d0a51f2d5a358f4a6c8b1a7c3ff74637.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/catchoftheday/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "c746b75fcbc30537066f",
+    "url": "/catchoftheday/static/css/main.4659352b.chunk.css"
+  },
+  {
+    "revision": "1ca3c4203b753b670683",
+    "url": "/catchoftheday/static/js/1.1ca3c420.chunk.js"
+  },
+  {
+    "revision": "b9fdfc2debfe4bec7246",
+    "url": "/catchoftheday/static/js/runtime~main.b9fdfc2d.js"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/catchoftheday/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/catchoftheday/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/catchoftheday/static/media/haymaker-webfont.e9535f20.ttf"
+  },
+  {
+    "revision": "c746b75fcbc30537066f",
+    "url": "/catchoftheday/static/js/main.c746b75f.chunk.js"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/catchoftheday/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/catchoftheday/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/catchoftheday/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/catchoftheday/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/catchoftheday/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "7faf52cd0a49430604dcf520b1110342",
+    "url": "/catchoftheday/index.html"
+  }
+];
