f4a4f89f295264998d79f32192edb306d0eba9cb
Michael Freytag <mtfreytag@gmail.com>
2019-01-25 15:12:22 -0600

    update favicon, Update Firebase imports

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import AddFishForm from './AddFishForm';
 import EditFishForm from './EditFishForm';
 import Login from './Login';
